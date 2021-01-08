f672ac7f13b4bfb97d073b9bbb9b1168c00a1736
rochhhelle <rochellemartins25@gmail.com>
2021-01-08 20:19:01 +0530

    Initial commit from create-react-app

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h2>MERN</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
