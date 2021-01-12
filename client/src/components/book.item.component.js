c09433ae5a181a94293bc197f7ffc37cdda485cd
rochhhelle <rochellemartins25@gmail.com>
2021-01-12 22:59:58 +0530

    Display authors in a list

--- client/src/components/book.item.component.js
+++ client/src/components/book.item.component.js
@@ -8,7 +8,13 @@ class Book extends Component {
       <tr>
         <td>{this.props.book.book_isbn}</td>
         <td>{this.props.book.book_name}</td>
-        <td>{this.props.book.book_author}</td>
+        <td>
+          <ul>
+            {this.props.book.book_author.map((i) => (
+              <li className="authors">{i}</li>
+            ))}
+          </ul>
+        </td>
         <td>{this.props.book.book_price}</td>
         <td>
           <Link to={"update/" + this.props.book._id}>Edit</Link>
